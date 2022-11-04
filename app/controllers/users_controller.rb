742ada6f5807f4a9bda41d7a8defa4282b028b32
ekatsnelson <elisek222@gmail.com>
2022-11-04 15:40:15 +0000

    users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def index
-    render({ :template => "user_templates/index.html.erb"})
-  end
+    matching_users = User.all
 
+    @list_of_users = matching_users.order({ :username => :asc })
+    render({ :template => "user_templates/index.html.erb" })
+  end
 end
